edbbc7c61d1b7e0e585d30d290bf263cb5faff8e
Anushka Singh <126336764+Anushka-2602@users.noreply.github.com>
2024-03-02 11:04:01 +0530

    Create GFG:FirstElementToOccurKTimes.java

---
+++ GFG:FirstElementToOccurKTimes.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    public int firstElementKTime(int n, int k, int[] a) { 
+        int[] hash= new int[10001];
+        
+        for(int i=0;i<n;i++){
+            hash[a[i]]++;         
+
+            if(hash[a[i]] >=k) return a[i];
+        }
+        
+        return -1;
+    } 
+}
